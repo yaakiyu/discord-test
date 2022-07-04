b3c40da1ee3fd272c07539d89bdca1aaae66912c
Yaakiyu <62779277+yaakiyu@users.noreply.github.com>
2022-07-04 03:42:29 +0000

    update: 初期コミット

---
+++ discord_test/__init__.py
@@ -0,0 +1,3 @@
+# discord test - test discord.py library
+
+__version__ = "0.0.1"
---
+++ discord_test/__main__.py
@@ -0,0 +1,12 @@
+# discord test - cli interface
+import sys
+
+
+def main() -> None:
+    print("Discord Test version 0.0.1 by yaakiyu")
+    print("Discord Test is a test program for discord.py library")
+
+
+if __name__ == '__main__':
+    main()
+    sys.exit(0)
