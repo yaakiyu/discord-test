32f62f8ed9c4f21d0f39fe6322db996505064268
Yaakiyu <62779277+yaakiyu@users.noreply.github.com>
2022-07-06 02:15:18 +0000

    update: ファイルロード作成。

--- discord_test/__main__.py
+++ discord_test/__main__.py
@@ -1,20 +1,32 @@
 # discord test - cli interface
 import discord_test
+
+import importlib
 import json
 
 
 def main() -> None:
     print(f"Discord Test version {discord_test.__version__} by yaakiyu loading...")
     config = discord_test.load_config()
+
+    import discord
+
     if not config["language"] in discord_test.configure.LANGUAGES:
         config["language"] = discord_test.configure.LANGUAGES[0]
-    
-    # load language file
-    with open("./languages/{}.json".format(config["language"]), "r") as f:
-        language = json.load(f)
 
-    if "default_mode" not in config:
-        ...
+    # 言語ファイル読み込み
+    with open(f"./languages/{config['language']}.json",
+              "r", encoding="utf-8") as file:
+        print_data = json.load(file)
+
+    print(print_data["welcome"])
+    run_file = input("file Path? ")
+    try:
+        module = importlib.import_module(
+            run_file.replace(".py", "").replace("/", ".").replace("\\", ".")
+        )
+    except ImportError:
+        return print(print_data["error_file"])
 
 
 if __name__ == '__main__':
