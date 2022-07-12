13585a20b3023a628c21f6461b16bbeb59216f81
Yaakiyu <62779277+yaakiyu@users.noreply.github.com>
2022-07-12 15:48:43 +0000

    update: こうすればいいやん

--- discord_test/__init__.py
+++ discord_test/__init__.py
@@ -1,7 +1,7 @@
 # discord test - test discord.py library
 
 from .configure import load_config, write_config
-from . import interpreter, types_
+from . import interpreter, types_, languages
 
 
 __version__ = "0.0.1"
