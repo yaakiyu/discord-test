51a1ad9ef4939883db180dedec59a2d532344f36
Yaakiyu <62779277+yaakiyu@users.noreply.github.com>
2022-07-12 15:23:38 +0000

    fix: aioconsole追加

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ setup(
     name="discord_test",
     packages=["discord_test"],
     version="0.0.1",
-    install_requires=["discord.py"],
+    install_requires=["discord.py", "aioconsole"],
     extras_require={},
     py_modules=["discord_test"]
 )
