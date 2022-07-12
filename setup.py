1a13496e5ae46b50fd31445adac7a01faa21abda
Yaakiyu <62779277+yaakiyu@users.noreply.github.com>
2022-07-12 15:52:43 +0000

    fix: 今度こそ。

--- setup.py
+++ setup.py
@@ -1,11 +1,11 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 
 setup(
     name="discord_test",
-    packages=["discord_test"],
+    packages=find_packages(),
     version="0.0.1",
     install_requires=["discord.py", "aioconsole"],
     extras_require={},
-    py_modules=["discord_test", "discord_test.languages"]
+    py_modules=["discord_test"]
 )
